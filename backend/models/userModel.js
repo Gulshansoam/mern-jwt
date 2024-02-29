47f39253522f969e851ab712d10d82acba0e2e74
gulshan <gulshansoam317@gmail.com>
2024-02-29 16:21:33 +0530

    Adding Validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
+const validator = require("validator");
 
 const userSchema = new Schema({
   email: {
@@ -16,6 +17,11 @@ const userSchema = new Schema({
 
 // static sign up method
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) throw Error("All fields must be filled");
+  if (!validator.isEmail(email)) throw Error("Email is not valid");
+  if (!validator.isStrongPassword(password))
+    throw Error("Password not strong enough");
+
   const exist = await this.findOne({ email });
   if (exist) throw Error("Email already in use");
   // hashing the password********
