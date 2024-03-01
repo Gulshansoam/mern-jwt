039531c477b2665a7e91dcbc65fda2b6596662a9
gulshan <gulshansoam317@gmail.com>
2024-03-01 11:17:43 +0530

    login done from backend

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,11 +2,18 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = (id) => {
- return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: "3d" });
+  return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: "3d" });
 };
 
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const signupUser = async (req, res) => {
